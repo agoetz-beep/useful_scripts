a6ea8432f6079be93988c99105630ef0ce427e31
agoetz <anna.goetz@student.kit.edu>
2021-07-06 11:25:25 +0200

    verbesserte Skripte

--- Vboson_Pt_Reweighting/root_files/canvas_divide_neu.py
+++ Vboson_Pt_Reweighting/root_files/canvas_divide_neu.py
@@ -7,7 +7,7 @@ from Definitions import var_info_dict
 boson=sys.argv[1]
 file_LO=ROOT.TFile.Open(sys.argv[2])
 file_NLO=ROOT.TFile.Open(sys.argv[3])
-file_factors=ROOT.TFile(boson + "_factors.root", "RECREATE")
+#file_factors=ROOT.TFile(boson + "_factors.root", "RECREATE")
 
 canvas=ROOT.TCanvas("canvas", "canvas", 4000, 3000)
 
@@ -15,15 +15,15 @@ legend=ROOT.TLegend(0.15,0.15)
 legend2=ROOT.TLegend(0.15,0.15)
 
 corr_factor_dict = { 
-                            "" : "",
-#                            "Vpt_corr_factor_incl" :" mit Korrekturfaktor (Vpt, inklusiver Phasenraum)",
-#                           "Vpt_theory_binning_corr_factor_incl" : " mit Korrekturfaktor (Vpt, inklusiver Phasenraum, Theorie-Binning)",
-#                            "Vpt_corr_factor_ana" : " mit Korrekturfaktor (Vpt, Analysephasenraum)",
-#                            "Vpt_theory_binning_corr_factor_ana" : " mit Korrekturfaktor (Vpt, Analysephasenraum, Theorie-Binning)",
-#                            "Vpt_Veta_corr_factor_incl" : " mit Korrekturfaktor (Vpt_Veta, inklusiver Phasenraum)",
-#                            "Vpt_DeltaPhi_corr_factor_incl" : " mit Korrekturfaktor (Vpt_Deltaphi, inklusiver Phasenraum)",
-#                            "Vpt_HT_corr_factor_incl" : " mit Korrekturfaktor (Vpt_HT, inklusiver Phasenraum)",
-#                            "Vpt_anzahl_corr_factor_incl" : " mit Korrekturfaktor (Vpt_anzahl, inklusiver Phasenraum)"
+#                            "" : "",
+                            "Vpt_corr_factor_incl" :" mit Korrekturfaktor (Vpt, inklusiver Phasenraum)",
+                            "Vpt_theory_binning_corr_factor_incl" : " mit Korrekturfaktor (Vpt, inklusiver Phasenraum, Theorie-Binning)",
+                            "Vpt_corr_factor_ana" : " mit Korrekturfaktor (Vpt, Analysephasenraum)",
+                            "Vpt_theory_binning_corr_factor_ana" : " mit Korrekturfaktor (Vpt, Analysephasenraum, Theorie-Binning)",
+                            "Vpt_Veta_corr_factor_incl" : " mit Korrekturfaktor (Vpt_Veta, inklusiver Phasenraum)",
+                            "Vpt_DeltaPhi_corr_factor_incl" : " mit Korrekturfaktor (Vpt_Deltaphi, inklusiver Phasenraum)",
+                            "Vpt_HT_corr_factor_incl" : " mit Korrekturfaktor (Vpt_HT, inklusiver Phasenraum)",
+                            "Vpt_anzahl_corr_factor_incl" : " mit Korrekturfaktor (Vpt_anzahl, inklusiver Phasenraum)"
                             
                         }
 
@@ -39,11 +39,11 @@ for key in var_info_dict:
             histo_dict["ratio"][key__+"_"+key+"_"+key_]=histo_dict["NLO"][key__+"_"+key+"_"+key_].Clone()
             histo_dict["ratio"][key__+"_"+key+"_"+key_].Divide(histo_dict["LO"][key__+"_"+key+"_"+key_])
             for key___ in histo_dict:
-                histo_dict[key__+"_"+key+"_"+key_].SetName(boson+"_"+key__+"_"+key+"_"+key_)
-                histo_dict[key__+"_"+key+"_"+key_].GetXaxis().SetTitle(var_info_dict[key]["Xaxis"])
-                histo_dict[key__+"_"+key+"_"+key_].GetXaxis().CenterTitle()
-                histo_dict[key__+"_"+key+"_"+key_].GetYaxis().SetTitle(var_info_dict[key]["Yaxis"])
-                histo_dict[key__+"_"+key+"_"+key_].GetYaxis().CenterTitle()
+                histo_dict[key___][key__+"_"+key+"_"+key_].SetName(boson+"_"+key__+"_"+key+"_"+key_)
+                histo_dict[key___][key__+"_"+key+"_"+key_].GetXaxis().SetTitle(var_info_dict[key]["Xaxis"])
+                histo_dict[key___][key__+"_"+key+"_"+key_].GetXaxis().CenterTitle()
+                histo_dict[key___][key__+"_"+key+"_"+key_].GetYaxis().SetTitle(var_info_dict[key]["Yaxis"])
+                histo_dict[key___][key__+"_"+key+"_"+key_].GetYaxis().CenterTitle()
             if var_info_dict[key]["dim"] == 1:
                 if key__ == "incl":
                     histo_dict["LO"][key__+"_"+key+"_"+key_].SetLineColor(1)
@@ -54,14 +54,14 @@ for key in var_info_dict:
                     histo_dict["NLO"][key__+"_"+key+"_"+key_].SetLineColor(6)
                     histo_dict["ratio"][key__+"_"+key+"_"+key_].SetLineColor(2)
                 histo_dict["LO"][key__+"_"+key+"_"+key_].SetTitle(var_info_dict[key]["Title"] + sel_func_dict[key__]["Title"] + corr_factor_dict[key_])
-                histo_dict["NLO"][key__+"_"+key+"_"+key_].SetTitle("")
+                histo_dict["ratio"][key__+"_"+key+"_"+key_].SetTitle("")
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetYaxis().SetTitle("Wikunsquerschnittsverhaeltnis NLO/LO")
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetYaxis().CenterTitle()
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetYaxis().SetRangeUser(0,2)
             if var_info_dict[key]["dim"] == 2:
                 for key___ in histo_dict:
-                    histo_dict[key__+"_"+key+"_"+key_].GetZaxis().SetTitle(var_info_dict[key]["Zaxis"])
-                    histo_dict[key__+"_"+key+"_"+key_].GetZaxis().CenterTitle()
+                    histo_dict[key___][key__+"_"+key+"_"+key_].GetZaxis().SetTitle(var_info_dict[key]["Zaxis"])
+                    histo_dict[key___][key__+"_"+key+"_"+key_].GetZaxis().CenterTitle()
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetZaxis().SetTitle("Wikunsquerschnittsverhaeltnis NLO/LO")
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetZaxis().CenterTitle()
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].GetZaxis().SetRangeUser(0,2)
@@ -69,7 +69,7 @@ for key in var_info_dict:
                 histo_dict["NLO"][key__+"_"+key+"_"+key_].SetTitle(" -NLO")
                 histo_dict["ratio"][key__+"_"+key+"_"+key_].SetTitle(" -Verhaeltnis NLO zu LO")
             histo_dict["ratio"][key__+"_"+key+"_"+key_].SetName(boson + "_"+ key_)
-            file_factors.WriteTObject(histo_dict["ratio"][key__+"_"+key+"_"+key_])
+            #file_factors.WriteTObject(histo_dict["ratio"][key__+"_"+key+"_"+key_])
                 
             
             
@@ -81,9 +81,9 @@ for key in var_info_dict:
                 legend.AddEntry(histo_dict["LO"][key__+"_"+key+"_"+key_], "LO_"+ key__ , "l")
                 legend.AddEntry(histo_dict["NLO"][key__+"_"+key+"_"+key_], "NLO_"+ key__, "l")
                 if key__ == "incl":
-                    legend.AddEntry(histo_dict["ratio"][key__+"_"+key+"_"+key_], "inklusiver Phasenraum", "l")
+                    legend2.AddEntry(histo_dict["ratio"][key__+"_"+key+"_"+key_], "inklusiver Phasenraum", "l")
                 if key__ == "ana":
-                    legend.AddEntry(histo_dict["ratio"][key__+"_"+key+"_"+key_], "Analysephasenraum", "l")
+                    legend2.AddEntry(histo_dict["ratio"][key__+"_"+key+"_"+key_], "Analysephasenraum", "l")
                 if key == "boson_pt" or key =="boson_pt_to1TeV" or key=="jets_fuehrende_Ordnung_pt" or key=="jets_HT" or key=="boson_transverse_mass" or key=="geladenes_lepton_pt" or key=="boson_invariant_mass" or key=="jets_NLO_pt" or key=="jets_NNLO_pt" or key=="jets_NNNLO_pt" or key=="jets_anzahl":
                     canvas.cd(1).SetLogy(1)
                 canvas.cd(1)
--- Vboson_Pt_Reweighting/root_files/entries.py
+++ Vboson_Pt_Reweighting/root_files/entries.py
@@ -6,7 +6,7 @@ from array import array
 canvas=ROOT.TCanvas("canvas", "canvas", 2000, 1500)
 boson=sys.argv[1]
 file_hadd=ROOT.TFile.Open(sys.argv[2])
-hist1=file_hadd.Get(boson+ "_boson_pt")
+hist1=file_hadd.Get(boson+ "_incl_boson_pt_")
 neue_bin_grenzen=[ 
     30,
     40,
@@ -29,14 +29,14 @@ neue_bin_grenzen=[
     500,
     650,
     1000]
-neue_bins=array("d", neue_bin_grenzen)
-neues_histogramm=hist1.Rebin(len(neue_bin_grenzen)-1,boson+ "_boson_pt_neue_grenzen",neue_bins)
-#entries=hist1.GetEntries()
-#print("Entries: " ,entries)
-neues_histogramm.GetXaxis().SetRangeUser(30,1000)
-neues_histogramm.Draw("histe")
-canvas.SetLogy(1)
-canvas.Print(boson + "_boson_pt_neue_grenzen.png")
+#neue_bins=array("d", neue_bin_grenzen)
+#neues_histogramm=hist1.Rebin(len(neue_bin_grenzen)-1,boson+ "_boson_pt_neue_grenzen",neue_bins)
+entries=hist1.GetEntries()
+print("Entries: " ,entries)
+#neues_histogramm.GetXaxis().SetRangeUser(30,1000)
+#neues_histogramm.Draw("histe")
+#canvas.SetLogy(1)
+#canvas.Print(boson + "_boson_pt_neue_grenzen.png")
 
 
 print("finished")
